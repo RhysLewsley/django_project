b4b0bda1921aac2e772dd801891ee8c49735f941
RhysLewsley <rhysl@example.com>
2025-11-18 19:17:44 +0000

    deployed to heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1(-=lr(pxh^9#yosg_c($qmd4z1hryo@2(#1asewgpm&c&bzv)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
